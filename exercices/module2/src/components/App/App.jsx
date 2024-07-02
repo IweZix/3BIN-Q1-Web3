9e2834bac0e9b8314fda4bbb3ca16c94ab6802a8
IweZix <IweZix@users.noreply.github.com>
2024-07-02 10:27:18 +0200

    refactor: module2 event

--- exercices/module2/src/components/App/App.jsx
+++ exercices/module2/src/components/App/App.jsx
@@ -4,29 +4,24 @@ import Button from "components/Button/Button"
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const changeCount = (delta) => {
+    if (delta === 0) {
+      return () => {
+        setCounter(0)
+      }
+    }
+    return () => {
+      setCounter(counter + delta)
+    }
   }
 
   return (
     <div>
       <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <Button changeCount={changeCount(+1)} text="plus" delta={1}/>
+      <Button changeCount={changeCount(0)} text="zero" delta={0}/>
+      <Button changeCount={changeCount(-1)} text="minus" delta={-1}/>
     </div>
   )
 } 
--- exercices/module2/src/components/Button/Button.jsx
+++ exercices/module2/src/components/Button/Button.jsx
@@ -1,7 +1,16 @@
-const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>
-        {text}
-    </button>
-)
+const Button = ({ changeCount, text, delta }) => {
+
+    const handleClick = (e) => {
+        const target = e.target.dataset.delta;
+        const delta = parseInt(target);
+        changeCount(delta);
+    }
+
+    return (
+        <button onClick={handleClick} data-delta={delta}>
+            {text}
+        </button>
+    );
+};
 
 export default Button;
