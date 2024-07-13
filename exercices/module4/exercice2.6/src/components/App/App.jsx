ae62e96ee6f94d54494d793ca67a81c15d359995
IweZix <IweZix@users.noreply.github.com>
2024-07-13 17:45:32 +0200

    add: module4 exercice2.6

---
+++ exercices/module4/exercice2.6/src/components/App/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+export const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+    if (newName === '') return
+
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName} >
+        <div>
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => 
+          <div key={person.name}>
+            {person.name}
+          </div>
+        )
+      }
+    </div>
+  )
+}
