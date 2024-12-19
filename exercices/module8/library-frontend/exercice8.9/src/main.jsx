dfcb399efcd5d2310dbe3ef1ced5552503ba923e
LucaN <lucanclv2@gmail.com>
2024-12-19 17:55:05 +0100

    add: module8 exercice8.9

---
+++ exercices/module8/library-frontend/exercice8.9/src/main.jsx
@@ -0,0 +1,39 @@
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Authors } from './components/Authors/Authors'
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client'
+import { Books } from './components/Books/Books'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Authors />,
+      },
+      {
+        path: "/books",
+        element: <Books />,
+      }
+    ],
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+)
